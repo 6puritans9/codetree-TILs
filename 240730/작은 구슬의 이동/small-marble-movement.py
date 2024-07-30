2671773923344cf6e3ad8f7e035997ae6ce09dd3
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-07-30 15:02:46 +0900

    Title: 작은 구슬의 이동, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240730/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,41 @@
+def in_range(x, y, n):
+    return x>0 and x<n or y>0 and y<n
+
+def turn(dir, n):
+    return ((n - 1) - dir)
+
+def move(dir, x, y):
+    nx = x + dx[dir]
+    ny = y + dy[dir]
+
+    return [nx, ny]
+
+n, t = [int(num) for num in input().split()]
+r, c, d = input().split()
+x, y = int(r) - 1, int(c) - 1
+
+dx = [0, 1, -1, 0]
+dy = [-1, 0, 0, 1]
+
+direction = {
+    "U": 1,
+    "R": 3,
+    "L": 0,
+    "D": 2
+    }
+dir = direction[d]
+is_turning = False
+
+while t:
+    if not in_range(x, y, n) and not is_turning:
+        dir = turn(dir, n)
+        is_turning = True
+    else:
+        if is_turning:
+            is_turning = False
+
+        x, y = move(dir, x, y)
+
+    t -= 1
+
+print(x + 1, y + 1, end=" ")
