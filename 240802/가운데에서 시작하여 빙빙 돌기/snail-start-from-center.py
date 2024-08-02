268652399fbb9778b5d7854924e619a93be0dffc
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-02 18:42:45 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 170ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240802/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -0,0 +1,41 @@
+def in_range(x, y, n):
+    return 0 <= x < n and 0<= y < n
+
+def fill(x, y, i):
+    table[x][y] = i
+
+def turn(dir):
+    return (dir + 1) % 4
+
+
+def move(x, y, dir):
+    nx = x + dxs[dir]
+    ny = y + dys[dir]
+
+    return [nx, ny]
+
+
+
+n = int(input())
+table = [[0 for _ in range(n)] for _ in range(n)]
+
+x, y = n // 2, n // 2
+dir = 0
+
+dxs = [0,-1,0,1]
+dys = [1,0,-1,0]
+
+for i in range(1, n**2 + 1):
+    fill(x, y, i)
+    nx, ny = move(x, y, dir)
+
+    if not in_range(nx,ny,n) or table[nx][ny]:
+        dir = turn(dir)
+        nx, ny = move(x, y, dir)
+    
+    x, y = nx, ny
+
+for row in table:
+    for col in row:
+        print(col, end=" ")
+    print()
