2d389d2645015f2d1d67d36609a1147940643df8
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-03 17:48:04 +0900

    Title: 체크판위에서 2, Time: 174ms, Memory: 26MB, Status: Passed - CodeTree

--- 240803/체크판위에서 2/on-the-checkboard-2.py
+++ 240803/체크판위에서 2/on-the-checkboard-2.py
@@ -1,13 +1,24 @@
-R, C = list(map(int, input().split()))
+def count_valid_paths(R, C, grid):
+    def is_valid_jump(x1, y1, x2, y2):
+        return x2 > x1 and y2 > y1 and grid[x1][y1] != grid[x2][y2]
+
+    def dfs(x, y, jumps):
+        if x == R-1 and y == C-1 and jumps == 3:
+            return 1
+        if jumps >= 3 or x >= R or y >= C:
+            return 0
+        
+        count = 0
+        for i in range(x+1, R):
+            for j in range(y+1, C):
+                if is_valid_jump(x, y, i, j):
+                    count += dfs(i, j, jumps+1)
+        return count
+
+    return dfs(0, 0, 0)
+
+R, C = map(int, input().split())
 grid = [input().split() for _ in range(R)]
 
-x, y = 0, 0
-start_color = grid[0][0]
-count = 0
-if start_color != grid[-1][-1]:
-    for i in range(x+1, R):
-        for j in range(y+1, C):
-            if grid[i][j] != start_color:
-                count += 1
-
-print(count)
+result = count_valid_paths(R, C, grid)
+print(result)
