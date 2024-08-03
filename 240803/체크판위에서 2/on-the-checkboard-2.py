7854d6b197f97d7017dcf2c3f129ed611528077c
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-03 18:04:30 +0900

    Title: 체크판위에서 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240803/체크판위에서 2/on-the-checkboard-2.py
+++ 240803/체크판위에서 2/on-the-checkboard-2.py
@@ -1,22 +1,26 @@
 def count_valid_paths(R, C, grid):
     def is_valid_jump(x1, y1, x2, y2):
-        return x2 > x1 and y2 > y1 and grid[x1][y1] != grid[x2][y2]
+        return x2>x1 and x2<R and grid[x1][y1] != grid[x2][y2]
 
-    def dfs(x, y, jumps):
-        if x == R-1 and y == C-1 and jumps == 3:
+    def dfs(x, y, jump):
+        if jump > 3 or x >= R or y >= C:
+            return
+        if jump ==3 and x == R-1 and y == C-1:
             return 1
-        if jumps >= 3 or x >= R or y >= C:
-            return 0
-        
+
         count = 0
         for i in range(x+1, R):
             for j in range(y+1, C):
-                if is_valid_jump(x, y, i, j):
-                    count += dfs(i, j, jumps+1)
+                if is_valid_jump(x,y,i,j):
+                    count += dfs(i,j,jump + 1)
+                
         return count
+            
+    return dfs(0,0,0)
 
-    return dfs(0, 0, 0)
 
+
+  
 R, C = map(int, input().split())
 grid = [input().split() for _ in range(R)]
 
