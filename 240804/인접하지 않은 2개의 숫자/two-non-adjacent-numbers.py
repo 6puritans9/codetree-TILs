86411eda625ab707034f2584c1ef774daf4e826f
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-04 18:12:04 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240804/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
+++ 240804/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -6,7 +6,7 @@ for i in range(n-2):
     for j in range(i+2, n):
         sum = numbers[i] + numbers[j]
     
-    if sum > max_sum:
-        max_sum = sum
+        if sum > max_sum:
+            max_sum = sum
 
 print(max_sum)
