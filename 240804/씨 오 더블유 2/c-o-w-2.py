9075a9e5f2f7f62569fe52c668c8e5422220ba96
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-04 18:38:10 +0900

    Title: 씨 오 더블유 2, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240804/씨 오 더블유 2/c-o-w-2.py
+++ 240804/씨 오 더블유 2/c-o-w-2.py
@@ -1,25 +1,29 @@
-def parse(_string):
-    indices = {
-        "C" : [],
-        "O" : [],
-        "W": []
-    }
+def count_COW_sequences(n, _string):
+    dpC = [0] * n
+    dpO = [0] * n
+    dpW = [0] * n
     
-    for i, char in enumerate(_string):
-        indices[char].append(i)
-
-    return indices
+    # Initialize the dpC array
+    dpC[0] = 1 if _string[0] == 'C' else 0
+    
+    for i in range(1, n):
+        dpC[i] = dpC[i - 1]
+        dpO[i] = dpO[i - 1]
+        dpW[i] = dpW[i - 1]
         
+        if _string[i] == 'C':
+            dpC[i] += 1
+        elif _string[i] == 'O':
+            dpO[i] += dpC[i - 1]
+        elif _string[i] == 'W':
+            dpW[i] += dpO[i - 1]
 
+    return dpW[-1]
+
+# Read input
 n = int(input())
 _string = input()
-indices = parse(_string)
 
-count = 0
-for c_idx in indices["C"]:
-    for o_idx in indices["O"]:
-        for w_idx in indices["W"]:
-            if c_idx < o_idx < w_idx:
-                count += 1
-
-print(count)
+# Compute the number of valid 'COW' sequences
+result = count_COW_sequences(n, _string)
+print(result)
