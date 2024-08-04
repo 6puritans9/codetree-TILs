2f2f3619535125ae24fa348c4e54bb8f3cff2484
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-04 17:45:27 +0900

    Title: 원 모양으로 되어있는 방, Time: 194ms, Memory: 25MB, Status: Passed - CodeTree

--- 240804/원 모양으로 되어있는 방/a-room-in-a-circle.py
+++ 240804/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -1,20 +1,12 @@
 def get_min_dist(N, peoples):
-    prefix_sum = [0] * N
-
-    for i in range(N):
-        if i == 0:
-            prefix_sum[i] = peoples[i]
-
-        prefix_sum[i] = prefix_sum[i - 1] + peoples[i]
-    
     min_dist = float("inf")
 
-    for start in range(N):
+    for end in range(N):
         total_dist = 0
 
-        for i in range(N):
-            dist = (i + start) % N
-            total_dist += peoples[i] * dist
+        for start in range(N):
+            dist = (start + N - end) % N
+            total_dist += peoples[start] * dist
 
         if min_dist > total_dist:
             min_dist = total_dist
