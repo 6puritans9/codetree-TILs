9f68b281053a72d733a931869998b4b457bdddbc
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-05 14:03:06 +0900

    Title: Carry 피하기 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240805/Carry 피하기 2/escaping-carry-2.py
+++ 240805/Carry 피하기 2/escaping-carry-2.py
@@ -1,14 +1,3 @@
-# def is_carry(num1, num2):
-#     sub_num1 = num1 % 10
-#     sub_num2 = num2 % 10
-#     if num1 < 10 or num2 < 10:
-#         return sub_num1 + sub_num2 >= 10
-
-#     if is_carry(num1 // 10, num2 // 10):
-#         return True
-    
-#     return sub_num1 + sub_num2 >= 10
-
 def is_carry(num1, num2):
     if num1 == 0 and num2 == 0:
         return False
@@ -20,38 +9,19 @@ def is_carry(num1, num2):
         return True
     
     return is_carry(num1 // 10, num2 // 10)
-    
 
+# Input reading
 n = int(input())
 numbers = [int(input()) for _ in range(n)]
 
 answer = -1
+
 for i in range(n-2):
-    # print(i)
     for j in range(i+1, n-1):
-        candidate = 0
-
         for k in range(j+1, n):
-            if is_carry(numbers[j], numbers[k]):
-                continue
-            else:
-                candidate = numbers[j] + numbers[k]
-                # print(f"i:{numbers[i]}")
-                # print(f"j:{numbers[j]}")
-                # print(f"k:{numbers[k]}")
-                # print(candidate)
-    
-        if is_carry(numbers[i], candidate):
-            # print(f"i:{numbers[i]}")
-            # print(f"candidate:{candidate}")
-            continue
-        else:
-            new_answer = numbers[i] + candidate
+            if not is_carry(numbers[i], numbers[j]) and not is_carry(numbers[j], numbers[k]) and not is_carry(numbers[i], numbers[k]):
+                candidate = numbers[i] + numbers[j] + numbers[k]
+                if candidate > answer:
+                    answer = candidate
 
-            if new_answer > answer:
-                answer = new_answer
-            # print(f"candidate:{candidate}")
-            # print(f"answer:{answer}")
-        
-        
 print(answer)
