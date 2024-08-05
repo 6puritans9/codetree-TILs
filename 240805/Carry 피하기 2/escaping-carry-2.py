bc8ceb9ed872d83fcf508ae1478884a5ece203ff
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-05 14:06:30 +0900

    Title: Carry 피하기 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240805/Carry 피하기 2/escaping-carry-2.py
+++ 240805/Carry 피하기 2/escaping-carry-2.py
@@ -1,14 +1,12 @@
 def is_carry(num1, num2):
-    if num1 == 0 and num2 == 0:
-        return False
-
-    sub_num1 = num1 % 10
-    sub_num2 = num2 % 10
-
-    if sub_num1 + sub_num2 >= 10:
-        return True
-    
-    return is_carry(num1 // 10, num2 // 10)
+    while num1 > 0 or num2 > 0:
+        sub_num1 = num1 % 10
+        sub_num2 = num2 % 10
+        if sub_num1 + sub_num2 >= 10:
+            return True
+        num1 //= 10
+        num2 //= 10
+    return False
 
 # Input reading
 n = int(input())
