3f26d794b81bf4adfe3e2c9745a6f5ce913f16c7
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-05 13:03:36 +0900

    Title: 1이 되는 순간까지, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240805/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ 240805/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -1,15 +1,12 @@
 def recursion(n):
     if n == 1:
-        return 1
+        return 0
     
-    if n % 2:
+    if n % 2 == 0:
         return 1+recursion(n//2)
     else:
         return 1+recursion(n//3)
 
 
 N = int(input())
-if N == 1:
-    print(0)
-else:
-    print(recursion(N))
+print(recursion(N))
