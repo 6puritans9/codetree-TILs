db93f870c86164fdd3f9807f066b553b5bc93992
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-06 16:27:32 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240806/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240806/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -1,11 +1,13 @@
 def find_max(numbers, n):
     index = n-1
+
     if index == 0:
         return numbers[index]
 
     cur_number = numbers[index]
+    nxt_number = find_max(numbers, n-1)
 
-    return cur_number if cur_number > find_max(numbers, n -1) else find_max(numbers, n -1)
+    return cur_number if cur_number > nxt_number else nxt_number
     
 
 
