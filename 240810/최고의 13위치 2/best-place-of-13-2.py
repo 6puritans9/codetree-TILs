3b90ac607b04a9bd6e49bc701357b548f4dc60c4
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-10 17:38:47 +0900

    Title: 최고의 13위치 2, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240810/최고의 13위치 2/best-place-of-13-2.py
+++ 240810/최고의 13위치 2/best-place-of-13-2.py
@@ -16,16 +16,15 @@ def find_max_coins(N, grid):
             for i2 in range(N):
                 if i1 == i2:
                     for j2 in range(N-2):
-                        if not j2+ 3 <= j1 or not j1 + 3 <= j2:
-                            continue
-                        
-                        max_coins = max(max_coins, coin_counts[i1][j1] + count(i2, j2))
+                        # if not j2+ 3 <= j1 or not j1 + 3 <= j2:
+                        if abs(j1 - j2) >= 3:
+                            max_coins = max(max_coins, coin_counts[i1][j1] + coin_counts[i2][j2])
                 else:
                     for j2 in range(N-2):
-                        max_coins = max(max_coins, coin_counts[i1][j1] + count(i2, j2))
+                        max_coins = max(max_coins, coin_counts[i1][j1] + coin_counts[i2][j2])
     
     return max_coins
-    
+
 
 N= int(input())
 grid = [[int(num) for num in input().split()] for _ in range(N)]
