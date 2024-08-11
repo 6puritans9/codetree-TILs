f4e973ad0883fe5b33bd9b03cda32c99eb5bb305
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-11 12:47:33 +0900

    Title: 구간 중 최대 합, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240811/구간 중 최대 합/max-sum-of-subarray.py
@@ -0,0 +1,12 @@
+def get_max_sum(numbers, size, sub_size):
+    max_sum = 0
+    for i in range(size - sub_size + 1):
+        max_sum = max(max_sum, sum(numbers[i:i+3]))
+
+    return max_sum
+
+
+n, k = list(map(int, input().split()))
+numbers = list(map(int, input().split()))
+
+print(get_max_sum(numbers, n, k))
