cedc73e7fa7acaacf64eefaa0ca8fa2e459d806f
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-11 12:47:59 +0900

    Title: 구간 중 최대 합, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240811/구간 중 최대 합/max-sum-of-subarray.py
+++ 240811/구간 중 최대 합/max-sum-of-subarray.py
@@ -1,7 +1,7 @@
 def get_max_sum(numbers, size, sub_size):
     max_sum = 0
     for i in range(size - sub_size + 1):
-        max_sum = max(max_sum, sum(numbers[i:i+3]))
+        max_sum = max(max_sum, sum(numbers[i:i+sub_size]))
 
     return max_sum
 
