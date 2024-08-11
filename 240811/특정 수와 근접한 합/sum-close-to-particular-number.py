c95b3e891c47c571853e7a2ee84718b5b12ae17a
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-11 12:08:24 +0900

    Title: 특정 수와 근접한 합, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 240811/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240811/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -1,19 +1,18 @@
 def get_candidate(numbers, i, j):
     return sum(numbers) - (numbers[i] + numbers[j])
 
-def get_closest_sum(target, numbers, length):
-    closest_sum = float("inf")
+def get_closest_diff(target, numbers, length):
+    difference = float("inf")
 
     for i in range(length-1):
         for j in range(i+1, length):
             candidate = get_candidate(numbers, i, j)
-            closest_sum = min(closest_sum, candidate)
+            difference = min(difference, abs(target - candidate))
 
-    difference = abs(target - closest_sum)
     return difference
 
 
 N, S = list(map(int, input().split()))
 numbers = list(map(int, input().split()))
 
-print(get_closest_sum(S, numbers, N))
+print(get_closest_diff(S, numbers, N))
