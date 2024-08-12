b1e3695e3b4ddc44d09af708d43ba8bdb347c9ef
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-12 23:06:40 +0900

    Title: 바구니 안의 사탕 2, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240812/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240812/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,6 +1,9 @@
 MAX_INDEX = 101
 
 N, K = list(map(int, input().split()))
+if K >= MAX_INDEX:
+    K = MAX_INDEX
+
 candies = [0] * MAX_INDEX
 
 for _ in range(N):
@@ -10,7 +13,7 @@ for _ in range(N):
     candies[pos] += amount
 
 max_sum = 0
-for i in range(1, (MAX_INDEX - K + 1)):
+for i in range(1, (MAX_INDEX - K + 1)+1):
     current_sum = sum(candies[i:i + (K * 2 + 1)])
 
     max_sum = max(max_sum, current_sum)
