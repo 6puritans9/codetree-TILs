256e4516b3fdfaa1506e68d4f8677adac9e692e2
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-12 19:12:18 +0900

    Title: 아름다운 수열 2, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240812/아름다운 수열 2/beautiful-sequence-2.py
+++ 240812/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,15 +1,17 @@
 def is_matching(target_seq, target_seq_length, sub_seq):
     checked = [0] * target_seq_length
-    
+
     for num1 in sub_seq:
         for j, num2 in enumerate(target_seq):
             if num1 == num2 and not checked[j]:
                 checked[j] = 1
+                break
 
     if sum(checked) == target_seq_length:
         return True
 
     return False
+
 
 N, M = list(map(int, input().split()))
 A = list(map(int, input().split()))
@@ -17,9 +19,9 @@ B = list(map(int, input().split()))
 count = 0
 
 for i in range(N - M + 1):
-    sub_seq = A[i:i+M]
+    sub_seq = A[i : i + M]
 
     if is_matching(B, M, sub_seq):
         count += 1
-        
+
 print(count)
