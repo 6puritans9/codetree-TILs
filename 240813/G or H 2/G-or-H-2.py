5084d08d54ef274b3bba5b425ac9b803230a2341
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-13 15:53:56 +0900

    Title: G or H 2, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240813/G or H 2/G-or-H-2.py
+++ 240813/G or H 2/G-or-H-2.py
@@ -6,33 +6,32 @@ def init(N):
         pos = int(pos)
         people.append((pos, char))
 
-    # Sort by position to ensure the correct order
     people.sort()
     
     return people
 
-def find_max_distance(people):
+
+def find_max_distance(people, people_length):
     max_distance = 0
-    num_people = len(people)
     
-    for i in range(num_people):
+    for i in range(N):
         g_count = 0
         h_count = 0
-        
-        for j in range(i, num_people):
-            if people[j][1] == 'G':
+
+        for j in range(i, N):
+            if people[j][1] == "G":
                 g_count += 1
             else:
                 h_count += 1
-            
+        
             if g_count == 0 or h_count == 0 or g_count == h_count:
-                distance = people[j][0] - people[i][0]
-                max_distance = max(max_distance, distance)
-                
+                max_distance = max(max_distance, people[j][0] - people[i][0])
+
     return max_distance
+
 
 N = int(input())
 people = init(N)
 
-max_distance = find_max_distance(people)
+max_distance = find_max_distance(people, N)
 print(max_distance)
