7ab388a23d480da14cd97824244d4d240babfc17
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-14 18:54:52 +0900

    Title: 개발팀의 능력, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240814/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 240814/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -1,9 +1,6 @@
 devs = list(map(int, input().split()))
 total_sum = sum(devs)
 
-# team1 = 0
-# team2 = 0
-
 min_diff = float("inf")
 for i in range(5):
     for j in range(i+1, 5):
@@ -11,14 +8,14 @@ for i in range(5):
         for k in range(5):
             for l in range(k+1, 5):
                 if i == k or i == l or j == k or j== l:
-                    break
+                    continue
 
                 team1 = devs[i] + devs[j]
                 team2 = devs[k] + devs[l]
                 single_dev = total_sum - (team1 + team2)
 
                 if team1 == team2 or team2 == single_dev or single_dev == team1:
-                    break
+                    continue
 
                 max_devs = max(team1, team2, single_dev)
                 min_devs = min(team1, team2, single_dev)
