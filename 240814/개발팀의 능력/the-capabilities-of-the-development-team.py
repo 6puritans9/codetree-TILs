e0417669c65f1a1d7f64b7ed7fb9b71e14abbfa8
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-14 18:28:04 +0900

    Title: 개발팀의 능력, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240814/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 240814/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -25,6 +25,9 @@ for i in range(5):
         
                 single_dev = total_sum - (team1 + team2)
 
+                if team1 == team2 or team2 == single_dev or single_dev == team1:
+                    break
+
                 max_devs = max(team1, team2, single_dev)
                 min_devs = min(team1, team2, single_dev)
 
