59776529719caa2cef7f28bb6d10b772a478cba4
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-15 16:51:49 +0900

    Title: 겹치지 않는 선분 2, Time: 125ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240815/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240815/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -5,20 +5,19 @@ def is_crossed(left_line, right_line):
     start_right_line = right_line[0]
     end_right_line = right_line[1]
 
-    if start_right_line < end_left_line and end_right_line <= end_left_line:
+    if start_left_line <= start_right_line < end_left_line and end_right_line <= end_left_line:
         return True
 
     return False
 
 N = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(N)]
-lines.sort()
 
 overlapping_lines = set()
 
 for i in range(N):
-    for j in range(i + 1, N):
-        if is_crossed(lines[i], lines[j]):
+    for j in range(N):
+        if i != j and is_crossed(lines[i], lines[j]):
             overlapping_lines.add(lines[i])
             overlapping_lines.add(lines[j])
 
