eb1db40d783f922db84d22812ab3b3f6e662a222
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-15 10:48:59 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240815/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -0,0 +1,27 @@
+N = int(input())
+dots = [[int(num) for num in input().split()] for _ in range(N)]
+
+area = 0
+for i in range(N):
+    min_width = float("inf")
+    min_height = float("inf")
+
+    xs = []
+    ys = []
+    for j in range(N):
+        if i == j:
+            continue
+
+        x, y = dots[j]
+        xs.append(x)
+        ys.append(y)
+
+    cur_width = max(xs) - min(xs)
+    cur_height = max(ys) - min(ys)
+
+    min_width = min(min_width, cur_width)
+    min_height = min(min_height, cur_height)
+
+    area = min_width * min_height
+
+print(area)
