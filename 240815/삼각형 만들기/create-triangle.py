eb3c0e8ae9856ac0e59748a5cb4165e309030e83
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-15 11:51:54 +0900

    Title: 삼각형 만들기, Time: 114ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240815/삼각형 만들기/create-triangle.py
@@ -0,0 +1,44 @@
+def get_width(x1, x2, x3, y1, y2, y3):
+    if not y1 == y2 == y3:
+        if y1 == y2:
+            return abs(x1 - x2)
+        elif y2 == y3:
+            return abs(x2 - x3)
+        else:
+            return abs(x3 - x1)
+    
+    return 0
+
+
+def get_height(x1,x2, x3, y1,y2, y3):
+    if not x1 == x2 == x3:
+        if x1 == x2:
+            return abs(y1 - y2)
+        elif x2 == x3:
+            return abs(y2 - y3)
+        else:
+            return abs(y3 - y1)
+    
+    return 0
+
+
+N = int(input())
+dots = [tuple(int(num) for num in input().split()) for _ in range(N)]
+
+max_area = 0
+for i in range(N):
+    max_width = 0
+    max_height = 0
+
+    for j in range(i+1, N):
+        for k in range(j+1, N):
+            x1, y1 = dots[i]
+            x2, y2 = dots[j]
+            x3, y3 = dots[k]
+
+            width = get_width(x1, x2, x3, y1, y2, y3)
+            height = get_height(x1, x2, x3, y1, y2, y3)
+
+            max_area = max(max_area, width*height)
+
+print(max_area)
