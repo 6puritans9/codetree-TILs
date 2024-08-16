5c7ae9b0410163b663284fc4bf4b9eb64f1d30f6
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-16 17:23:00 +0900

    Title: 상해버린 치즈, Time: 133ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240816/상해버린 치즈/rotten-cheese.py
+++ 240816/상해버린 치즈/rotten-cheese.py
@@ -11,11 +11,10 @@ sick_info = [[int(num) for num in input().split()] for _ in range(sick_people)]
 
 # 2. find those who had eaten bad cheese
 # 3. count the number of people(get rid of duplicates)
-# 4. 
+# 4. validate innocent cheese which sick people didn't eat
 
 # Get the bad cheese
 bad_cheese_candidates = [0] * (cheese + 1)
-innocent_cheese = [1] * (cheese + 1)
 for info_1 in sick_info:
     sick_people_idx, sick_time = info_1
     
@@ -24,15 +23,16 @@ for info_1 in sick_info:
 
         if people_idx == sick_people_idx and eat_time < sick_time:
             bad_cheese_candidates[cheese_idx] += 1
-        else:
-            innocent_cheese[cheese_idx] = 0
+    
+# print(bad_cheese_candidates)
 
 sick_people_candidates = set()
 # Find who else had the bad cheese
 for info in cheese_info:
     people_idx, cheese_idx, eat_time = info
     
-    if bad_cheese_candidates[cheese_idx] > 0 and not innocent_cheese[cheese_idx]:
+    if bad_cheese_candidates[cheese_idx] >= sick_people:
         sick_people_candidates.add(people_idx)
-    
+
+# print(sick_people_candidates)
 print(len(sick_people_candidates))
