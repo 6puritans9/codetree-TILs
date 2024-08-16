7e897222e044618e6b78f09fa45a8da6d1d3b592
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-16 16:56:42 +0900

    Title: 상해버린 치즈, Time: 136ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240816/상해버린 치즈/rotten-cheese.py
@@ -0,0 +1,34 @@
+people, cheese, cheese_eaten, sick_people = tuple(map(int, input().split()))
+
+# people_idx, cheese_idx, eat_time
+cheese_info = [[int(num) for num in input().split()] for _ in range(cheese_eaten)]
+# people_idx, sick_time
+sick_info = [[int(num) for num in input().split()] for _ in range(sick_people)]
+
+# how to solve
+# 1. validate bad cheese
+# 1-1. it can be narrowed down to less than sick people's eat_time(cheeses_eaten_idx)
+
+# 2. find those who had eaten bad cheese
+# 3. count the number of people(get rid of duplicates)
+
+# Get the bad cheese
+bad_cheese_candidates = [0] * (cheese + 1)
+for info_1 in sick_info:
+    sick_people_idx, sick_time = info_1
+    
+    for info_2 in cheese_info:
+        people_idx, cheese_idx, eat_time = info_2
+
+        if people_idx == sick_people_idx and eat_time < sick_time:
+            bad_cheese_candidates[cheese_idx] += 1
+
+sick_people_candidates = set()
+# Find who else had the bad cheese
+for info in cheese_info:
+    people_idx, cheese_idx, eat_time = info
+    
+    if bad_cheese_candidates[cheese_idx] > 0:
+        sick_people_candidates.add(people_idx)
+    
+print(len(sick_people_candidates))
