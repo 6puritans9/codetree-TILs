af8145fcefc99632a978e329faeae027c49cc308
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-16 18:09:22 +0900

    Title: 이상한 폭탄 2, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240816/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,19 @@
+def in_range(i, N):
+    return 0 <= i < N
+
+
+N, K = map(int, input().split())
+bombs = [int(input()) for _ in range(N)]
+
+max_num = 0
+for i, bomb_1 in enumerate(bombs):
+    for j in range(i+1, i+4):
+        if not in_range(j, N):
+            break
+
+        bomb_2 = bombs[j]
+
+        if bomb_1 == bomb_2:
+            max_num = max(max_num, bomb_1)
+
+print(max_num)
