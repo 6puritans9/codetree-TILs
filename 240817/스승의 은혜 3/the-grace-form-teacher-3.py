461b8594ae4b4190beb038db8450eafa6d4429ac
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-17 16:03:14 +0900

    Title: 스승의 은혜 3, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240817/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,30 @@
+N, B = map(int, input().split())
+# price P(i); shipping S(i) per student
+table = [tuple(map(int, input().split()))for _ in range(N)]
+
+max_count = 0
+for i in range(N):
+    budget = B
+    count = 0
+    
+    for j in range(N):
+        price, shipping = table[j][0], table[j][1]
+        current_cost = price + shipping
+
+        if budget < current_cost:
+            break
+
+        if i == j:
+            discounted = price // 2
+            current_cost = discounted + shipping
+            
+            budget -= current_cost
+            count += 1
+            continue
+        
+        budget -= (price + shipping)
+        count += 1
+
+    max_count = max(max_count, count)
+
+print(max_count)
