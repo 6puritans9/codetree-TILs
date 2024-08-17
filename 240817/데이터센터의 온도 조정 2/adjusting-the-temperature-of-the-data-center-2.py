fb54b0cf7959c41e24b78ef583a85bd59ac7e31a
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-17 17:56:14 +0900

    Title: 데이터센터의 온도 조정 2, Time: 171ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240817/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,29 @@
+def get_work(temp, machine, C, G, H):
+    min_temp, max_temp = machine
+    
+    if temp < min_temp:
+        return C
+    elif min_temp <= temp < max_temp:
+        return G
+    else:
+        return H
+
+
+N, C, G, H = map(int, input().split())
+machines = [tuple(map(int, input().split())) for _ in range(N)]
+
+start, end = 1001, -1
+for machine in machines:
+    start = min(start, machine[0])
+    end = max(end, machine[1])
+
+max_total_work = 0
+for temp in range(start, end + 1):
+    total_work = 0
+    
+    for machine in machines:
+        total_work += get_work(temp, machine, C, G, H)
+    
+    max_total_work = max(max_total_work, total_work)
+
+print(max_total_work)
