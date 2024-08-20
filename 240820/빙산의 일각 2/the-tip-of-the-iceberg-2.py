1c70cd6ae0598f856e2f631a0d8b408be5d154a4
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-20 19:06:58 +0900

    Title: 빙산의 일각 2, Time: 112ms, Memory: 25MB, Status: Passed - CodeTree

--- 240820/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240820/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -6,14 +6,14 @@ min_height = min(Hs)
 
 max_icebergs = 0
 for surface_height in range(max_height, min_height - 1, -1):
-    new_Hs = [h - surface_height for h in Hs]
     count = 0
 
-    for i in range(N):
-        if new_Hs[i] > 0 and i == N - 1 or new_Hs[i] > 0 and new_Hs[i+1] <= 0:
+    if Hs[0] > surface_height:
+        count += 1
+    
+    for i in range(1, N):
+        if Hs[i] > surface_height and Hs[i-1] <= surface_height:
             count += 1
-        else:
-            continue
 
     max_icebergs = max(max_icebergs, count)
 
