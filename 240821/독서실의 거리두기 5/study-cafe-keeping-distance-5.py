e437725a27565367a59e80ef7e6ac7fd7da0268c
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-21 22:16:30 +0900

    Title: 독서실의 거리두기 5, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240821/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -0,0 +1,16 @@
+N = int(input())
+seats = list(map(int, input()))
+
+max_distance = 0
+empty_seats = []
+for i, seat in enumerate(seats):
+
+    if not seat:
+        empty_seats.append(0)
+    elif seat and len(empty_seats) or i == N-1 and len(empty_seats):
+        distance = (len(empty_seats) // 2) + 1
+        max_distance = max(max_distance, distance)
+        empty_seats.clear()
+   
+
+print(max_distance)
