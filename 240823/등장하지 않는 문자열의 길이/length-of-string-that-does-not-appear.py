5adc11df7b3344f03b77de195aafd85b189d087a
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-23 09:21:32 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240823/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -0,0 +1,27 @@
+def compare_string(_string, N):
+    length = 1
+    half_string_length = (N // 2) + 1
+
+    answer = 0
+    while length <= half_string_length:
+        for j in range(N - length):
+            target_string = _string[j:j + length]
+
+            for k in range(j + 1, (j + 1)+length):
+                cur_string = _string[j+1:j+1+length]
+
+                if j + 1 + length > N - length or target_string == cur_string:
+                    break
+                
+                answer = length
+        
+        length += 1
+
+    return answer
+
+
+N = int(input())
+_string = [char for char in input()]
+
+answer = compare_string(_string, N)
+print(answer + 1)
