5c09184ab7176ebf91745cb9572c34d5bed86333
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-23 13:33:32 +0900

    Title: 가장 많이 나온 쌍, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240823/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+ranges = [[int(num) for num in input().split()] for _ in range(m)]
+        
+frequency = [0] * (n + 1)
+for _range in ranges:
+    x, y = _range
+    frequency[x] += 1
+    frequency[y] += 1
+
+max_i, second_i = max(frequency), 0
+for i, value in enumerate(frequency, start = 1):
+    if second_i < value < max_i:
+        second_i = value
+
+print(second_i)
