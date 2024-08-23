c9fcd75772b4d0d303749896caf35d89a55af5f8
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-23 14:36:09 +0900

    Title: 숫자들의 최대 차, Time: 166ms, Memory: 28MB, Status: Passed - CodeTree

--- 240823/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 240823/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -1,17 +1,13 @@
 N, K = map(int, input().split())
 numbers = [int(input()) for _ in range(N)]
+numbers.sort()
 
 max_count = 0
-for i in range(N - 1):
-    number1 = numbers[i]
-    count = 0
+start = 0
 
-    for j in range(N - 1):
-        number2 = numbers[j]
-
-        if abs(number1 - number2) <= K:
-            count += 1
-
-    max_count = max(max_count, count)
+for end in range(N):
+    while numbers[end] - numbers[start] > K:
+        start += 1
+    max_count = max(max_count, end - start + 1)
 
 print(max_count)
