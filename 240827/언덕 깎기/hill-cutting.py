557bfb9e38c2ecda9ad0a010a6e4163defe3ffc4
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-27 11:28:02 +0900

    Title: 언덕 깎기, Time: 110ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240827/언덕 깎기/hill-cutting.py
+++ 240827/언덕 깎기/hill-cutting.py
@@ -2,13 +2,20 @@ import math
 
 N = int(input())
 hills = [int(input()) for _ in range(N)]
+hills.sort()
 
 min_height, max_height = min(hills), max(hills)
 
 answer = math.inf
 for height in range(min_height, max_height + 1):
+    total_cost = 0
+
     if (max_height - height) - (min_height + height) <= 17:
-        cost = pow(height,2) * 2
+        for hill in hills:
+            cost = pow(height,2) * 2
+
+            if hill == min_height or hill == max_height:
+                total_cost += cost
             
         answer = min(answer, cost)
 
