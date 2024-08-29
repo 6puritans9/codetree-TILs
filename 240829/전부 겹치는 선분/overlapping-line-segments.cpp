d2d3ea3a7da1fb35aecd98bc083c7b9c77067977
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-29 20:36:20 +0900

    Title: 전부 겹치는 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240829/전부 겹치는 선분/overlapping-line-segments.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int n;
+int x1, x2;
+int max_x1;
+int min_x2 = INT_MAX;
+
+
+int main() {
+    cin >> n;
+
+    for (int i =0; i < n; i++){
+        cin >> x1 >> x2;
+        max_x1 = max(max_x1, x1);
+        min_x2 = min(min_x2, x2);
+    }
+
+    if (max_x1 <= min_x2)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
