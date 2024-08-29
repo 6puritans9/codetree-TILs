9c1ecff1a5133a4dd4aa0a6731b6a94fe151854c
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-29 19:53:38 +0900

    Title: 구역 청소, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240829/구역 청소/district-cleaning.py
+++ 240829/구역 청소/district-cleaning.py
@@ -1,7 +1,7 @@
 def get_intersection(a,b,c,d):
-    if c < b and a < c:
+    if c < b and a < c and d>b:
         return b-c
-    elif a < d and c < a:
+    elif a < d and c < a and b>d:
         return d-a
     elif a < c and d < b:
         return d-c
