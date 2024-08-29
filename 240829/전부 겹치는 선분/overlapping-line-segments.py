241c6daac49e8916c3fdebe0f772c30ad1833b59
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-29 20:22:13 +0900

    Title: 전부 겹치는 선분, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240829/전부 겹치는 선분/overlapping-line-segments.py
+++ 240829/전부 겹치는 선분/overlapping-line-segments.py
@@ -1,18 +1,22 @@
 def does_intersect(start, end, lines):
-    for k in range(start, end + 1):
-        for line in lines:
-            if line[0] > k or k < line[1]:
-                break
-        return True
-    
-    return False
+    for line in lines:
+        if line[0] > k or k < line[1]:
+            return False
+
+    return True
 
 
 n = int(input())
 lines = [tuple(int(num) for num in input().split()) for _ in range(n)]
 
 start, end = min(lines[0]), max(lines[1])
-intersection = does_intersect(start, end, lines)
+intersection = False
+
+for k in range(start, end + 1):
+    intersection = does_intersect(start, end, lines)
+    if intersection:
+        break
+
 if intersection:
     print("Yes")
 else:
