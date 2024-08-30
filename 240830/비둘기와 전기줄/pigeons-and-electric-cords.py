5d504b893f910f84397e09d166bc705e3a61c164
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-30 10:48:42 +0900

    Title: 비둘기와 전기줄, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240830/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+pigeons = [-1] * (N + 1)
+states = [[int(num) for num in input().split()] for _ in range(N)]
+
+total_move = 0
+for state in states:
+    pigeon, pos = state
+
+    if pigeons[pigeon] == -1:
+        pigeons[pigeon] = pos
+    elif pigeons[pigeon] != pos:
+        total_move += 1
+    
+print(total_move)
