f277c70ba5871f5d285a1a6f16e124bd9b3927aa
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-31 14:14:02 +0900

    Title: 연속된 숫자 만들기 2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240831/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240831/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,8 +1,16 @@
-a, b, c = sorted(map(int, input().split()))
+def min_movements(positions):
+    gaps = [positions[1] - positions[0], positions[2] - positions[1]]
 
-if c - a == 2:
-    print(0)
-elif b - a <= 2 or c - b <= 2:  # Can be made consecutive with 1 move
-    print(1)
-else:
-    print(2)
+    if gaps[0] == 1 and gaps[1] == 1:
+        return 0
+
+    if gaps[0] == 2 or gaps[1] == 2:
+        return 1
+
+    return 2
+
+
+positions = list(map(int, input().split()))
+positions.sort()
+
+print(min_movements(positions))
