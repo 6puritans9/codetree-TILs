0dfe69f191dee0e3d2bc33253551229c748eae80
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-31 16:15:52 +0900

    Title: 개발자의 가위바위보, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240831/개발자의 가위바위보/developer's-rock-paper-scissors.py
+++ 240831/개발자의 가위바위보/developer's-rock-paper-scissors.py
@@ -1,16 +1,10 @@
 N = int(input())
 pairs = [[int(num) for num in input().split()] for _ in range(N)]
 
-count1 = 0
-count2 = 0
+wins = 0
 for pair in pairs:
     a, b = pair
-    if a == b:
-        continue
+    if a == b-1 or a == b+1:
+        wins += 1
 
-    if a > b:
-        count1 += 1
-    else:
-        count2 += 1
-
-print(max(count1, count2))
+print(wins)
