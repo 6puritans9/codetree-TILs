fbf8ec4b8018f94b1053e3c292990e03d4d58c7e
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-31 13:26:50 +0900

    Title: 연속된 숫자 만들기 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240831/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240831/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -7,8 +7,10 @@ count = 0
 while dist1 > 1 or dist2 > 1:
     if dist1 > dist2:
         a = (b + c) // 2
+        a, b = b, a
     else:
         c = (a + b) // 2
+        c, b = b, c
 
     dist1 = b - a
     dist2 = c - b
