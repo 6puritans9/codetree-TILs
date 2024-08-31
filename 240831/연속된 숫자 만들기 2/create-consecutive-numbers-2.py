d1c98af55dd34fd86f151c814d6c6b522b3e9732
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-08-31 14:12:06 +0900

    Title: 연속된 숫자 만들기 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240831/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240831/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,28 +1,8 @@
-a, b, c = map(int, input().split())
+a, b, c = sorted(map(int, input().split()))
 
-dist1 = b - a
-dist2 = c - b
-
-count = 0
-while dist1 > 1 or dist2 > 1:
-    if dist1 == 1:
-        a = b - 2
-        a, b = b, a
-
-    elif dist2 == 1:
-        c = b - 2
-        c, b = b, c
-
-
-    elif dist1 > dist2:
-        a = (b + c) // 2
-        a, b = b, a
-    else:
-        c = (a + b) // 2
-        c, b = b, c
-
-    dist1 = b - a
-    dist2 = c - b
-    count += 1
-
-print(count)
+if c - a == 2:
+    print(0)
+elif b - a <= 2 or c - b <= 2:  # Can be made consecutive with 1 move
+    print(1)
+else:
+    print(2)
