a564503f80ce545f1898ed55b7fb447bbc08e06d
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-09-01 16:53:43 +0900

    Title: 개발자의 가위바위보, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240901/개발자의 가위바위보/developer's-rock-paper-scissors.py
+++ 240901/개발자의 가위바위보/developer's-rock-paper-scissors.py
@@ -1,10 +1,28 @@
+def did_a_win(a, b):
+    if a == "r" and b == "s" or a == "s" and b == "p" or a == "p" and b == "r":
+        return True
+
+    return False
+
+
 N = int(input())
 numbers = [tuple(int(num) for num in input().split()) for _ in range(N)]
+mappings = [("r", "p", "s"), ("r", "s", "p"), ("s", "r", "p"), ("s", "p", "r"), ("p", "r", "s"), ("p", "s", "r")]
 
-wins = 0
-for number in numbers:
-    i, j = number
-    if i == (j % 3) + 1:
-        wins += 1
+max_wins = 0
+for mapping in mappings:
+    cur_wins = 0
 
-print(wins)
+    for a, b in numbers:
+        if a == b:
+            continue
+
+        map_a = mapping[a - 1]
+        map_b = mapping[b - 1]
+
+        if did_a_win(map_a, map_b):
+            cur_wins += 1
+
+    max_wins = max(max_wins, cur_wins)
+
+print(max_wins)
