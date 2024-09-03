f4d589f766f0adc0cce2192ab4824e52c17b54bf
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-09-03 18:16:05 +0900

    Title: 두 번째로 작은 수의 위치, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240903/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -0,0 +1,24 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+
+# pos[number][0] = index
+# pos[number][1] = counts
+results = [[None, 0] for _ in range(101)]
+
+
+ans = -1
+for i, number in enumerate(numbers):
+    if not results[number][0]:
+        results[number][0] = i + 1
+        results[number][1] += 1
+    else:
+        results[number][1] += 1
+
+did_pass_first = False
+for result in results:
+    if not did_pass_first and result[0]:
+        did_pass_first = True
+    elif did_pass_first and result[0] and result[1] == 1:
+        ans = result[0]
+
+print(ans)
