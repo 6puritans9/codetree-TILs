e2e083c5ebe54fe9770ff4b5c078f14faf62ac7c
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-09-03 17:35:38 +0900

    Title: 독서실의 거리두기 2, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240903/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240903/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -1,42 +1,25 @@
-import sys
-import math
+def max_min_distance(N, seats):
+    # Convert the string into a list of indices where seats are occupied
+    occupied = [i for i in range(N) if seats[i] == '1']
+    
+    max_dist = 0
+    
+    # Check the distance we can achieve by placing the new person at either end
+    # Check at the beginning (before the first '1')
+    max_dist = max(max_dist, occupied[0])
+    
+    # Check in between each pair of consecutive '1's
+    for i in range(1, len(occupied)):
+        max_dist = max(max_dist, (occupied[i] - occupied[i - 1]) // 2)
+    
+    # Check at the end (after the last '1')
+    max_dist = max(max_dist, N - 1 - occupied[-1])
+    
+    return max_dist
 
-
-def is_adjacent(i, seats):
-    if seats[i - 1] and seats[i]:
-        return True
-
-    return False
-
-
-def get_min_dist(i, seats, n):
-    dist = math.inf
-
-    for i in range(n):
-        for j in range(i + 1, n):
-            if seats[i] and seats[j]:
-                dist = min(dist, j - i)
-
-    return dist
-
-
+# Read inputs
 N = int(input())
-seats = [int(num) for num in input()]
-max_min_dist = 0
+seats = input().strip()
 
-for i in range(N):
-    if is_adjacent(i, seats):
-        print(1)
-        sys.exit(0)
-
-    if i == 0 and not seats[i] and not seats[i+1] or i == N-1 and not seats[N-1] and not seats[N-2]:
-        seats[i] = 1
-        max_min_dist = max(max_min_dist, get_min_dist(i, seats, N))
-        seats[i] = 0
-
-    elif not seats[i] and not seats[i-1] and not seats[i+1]:
-        seats[i] = 1
-        max_min_dist = max(max_min_dist, get_min_dist(i, seats, N))
-        seats[i] = 0
-
-print(max_min_dist)
+# Print the result
+print(max_min_distance(N, seats))
