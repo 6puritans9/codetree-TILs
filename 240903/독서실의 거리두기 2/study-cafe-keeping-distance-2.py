e6a206ae8b19b618f0ef3cff3d3d415b2878c052
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-09-03 18:01:50 +0900

    Title: 독서실의 거리두기 2, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240903/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240903/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -1,25 +1,58 @@
-def max_min_distance(N, seats):
-    # Convert the string into a list of indices where seats are occupied
-    occupied = [i for i in range(N) if seats[i] == '1']
-    
-    max_dist = 0
-    
-    # Check the distance we can achieve by placing the new person at either end
-    # Check at the beginning (before the first '1')
-    max_dist = max(max_dist, occupied[0])
-    
-    # Check in between each pair of consecutive '1's
-    for i in range(1, len(occupied)):
-        max_dist = max(max_dist, (occupied[i] - occupied[i - 1]) // 2)
-    
-    # Check at the end (after the last '1')
-    max_dist = max(max_dist, N - 1 - occupied[-1])
-    
-    return max_dist
+import sys
 
-# Read inputs
-N = int(input())
-seats = input().strip()
+n = int(input())
+seats = [int(num) for num in input()]
 
-# Print the result
-print(max_min_distance(N, seats))
+max_dist = 0
+first_idx, last_idx = None, None
+for i in range(n):
+    if seats[i]:
+        for j in range(i+1, n):
+            if seats[j]:
+                cur_dist = j - i
+
+                if cur_dist > max_dist:
+                    max_dist = cur_dist
+                    first_idx, last_idx = i, j
+
+                break
+
+max_dist2 = 0
+far_side_idx = None
+if not seats[0]:
+    dist = 0
+    for i in range(0, n):
+        if seats[i]:
+            break
+        dist += 1
+    
+    if dist > max_dist2:
+        max_dist2 = dist
+        far_side_idx = 0
+
+if not seats[n-1]:
+    dist = 0
+    for i in range(n-1, -1, -1):
+        if seats[i]:
+            break
+        dist += 1
+    
+    if dist > max_dist2:
+        max_dist2 = dist
+        far_side_idx = n-1
+
+if max_dist2 >= max_dist // 2:
+    seats[far_side_idx] = 1
+else:
+    seats[(first_idx + last_idx) // 2] = 1
+
+min_max_dist = float("inf")
+for i in range(n):
+    if seats[i]:
+        for j in range(i+1, n):
+            if seats[j]:
+                min_max_dist = min(min_max_dist, j-i)
+
+                break
+
+print(min_max_dist)
