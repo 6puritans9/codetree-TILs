1095470a541f7eeeb9306b05dc1bec1cb151a015
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-09-05 18:58:31 +0900

    Title: 세 수의 최대 곱, Time: 133ms, Memory: 31MB, Status: Passed - CodeTree

--- 240905/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240905/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,14 +1,26 @@
 n = int(input())
 integers = list(map(int, input().split()))
+integers.sort()
 
 ans = -float("inf")
+zero_idx = None
+positive_idx = None
 for i in range(n):
-    for j in range(i+1, n):
-        for k in range(j+1, n):
-            computation = integers[i] * integers[j] * integers[k]            
-            # if computation < 0:
-            #     continue
-            
-            ans = max(ans, computation)
+    if integers[i] == 0:
+        zero_idx = i
+
+    elif integers[i] > 0:
+        positive_idx = i
+        break
+
+if not positive_idx and not zero_idx:
+    ans = intgers[-1] * integers[-2] * integers[-3]
+elif not positive_idx and zero_idx:
+    ans = 0
+elif positive_idx > 1:
+    ans = max(integers[0] * integers[1] * integers[-1], integers[-1] * integers[-2] * integers[-3])
+elif positive_idx >= 0:
+    ans = integers[-1] * integers[-2] * integers[-3]
+
 
 print(ans)
