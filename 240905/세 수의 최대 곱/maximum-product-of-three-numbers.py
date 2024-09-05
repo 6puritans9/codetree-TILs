43ea315d7d8c5f4b79eeaeab6d475486153dcfda
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-09-05 18:46:59 +0900

    Title: 세 수의 최대 곱, Time: 5053ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

--- 240905/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240905/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -6,8 +6,8 @@ for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
             computation = integers[i] * integers[j] * integers[k]            
-            if computation < 0:
-                continue
+            # if computation < 0:
+            #     continue
             
             ans = max(ans, computation)
 
