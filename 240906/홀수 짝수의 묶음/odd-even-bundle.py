5b82d8e675d5a6fed646a732b1e46db80e0e395f
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-09-06 09:03:16 +0900

    Title: 홀수 짝수의 묶음, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240906/홀수 짝수의 묶음/odd-even-bundle.py
@@ -0,0 +1,26 @@
+N = int(input())
+numbers = list(map(int, input().split()))
+
+odds = []
+len_odds = 0
+evens = []
+len_evens = 0
+for number in numbers:
+    if number % 2:
+        odds.append(number)
+        len_odds += 1
+    else:
+        evens.append(nuamber)
+        len_evens += 1
+
+ans = 0
+if not len_evens:
+    ans = len_odds // 2
+elif len_evens < len_odds:
+    ans = len_odds
+else:
+    ans = len_evens + len_odds
+# elif len_evens > len_odds:
+#     ans = len_evens + len_odds
+
+print(ans)
