478b38d547b01175e0e5513b3a127631be7ae687
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-09-11 17:34:30 +0900

    Title: 2개씩 그룹짓기 2, Time: 114ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240911/2개씩 그룹짓기 2/group-of-pairs-2.py
+++ 240911/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -2,16 +2,11 @@ n = int(input())
 numbers = list(map(int, input().split()))
 length = n*2
 
-max_min = -1
-for i in range(length):
-    min_diff = float("inf")
-    
-    for j in range(length):
-        if i == j:
-            continue
-        
-        min_diff = min(min_diff, abs(numbers[i] - numbers[j]))
-    
-    max_min = max(max_min, min_diff)
+max_min = 0
+numbers.sort()
+for i in range(1, length):
+    diff = numbers[i] - numbers[i-1]
+
+    max_min = max(max_min, diff)
 
 print(max_min)
