cdf650c305189657997183e0aa054b14cba27a7d
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-09-18 21:00:50 +0900

    Title: 황금비율 토스트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240918/황금비율 토스트/golden-toast.py
@@ -0,0 +1,61 @@
+from collections import deque
+
+
+def move(pos, instruction):
+    global letters_left
+    global letters_right
+    global n
+    new_pos = pos
+
+    if instruction == "L" and pos != 0:
+        letters_right.appendleft(letters_left.pop())
+        new_pos -= 1
+    elif instruction == "R" and pos != n:
+        letters_left.append(letters_right.popleft())
+        new_pos += 1
+
+    return new_pos
+
+
+def delete(pos):
+    global letters_right
+    global n
+
+    if pos != n:
+        letters_right.popleft()
+
+
+def insert(pos, letter):
+    global letters_left
+    global n
+    
+    letters_left.append(letter)
+    n += 1
+    new_pos = pos + 1
+    
+    return new_pos
+
+
+n, m = map(int, input().split())
+letters_left = deque([char for char in input()])
+letters_right = deque()
+pos = n
+
+for _ in range(m):
+    _input = input().split()
+
+    if len(_input) > 1:
+        instruction, letter = _input
+    else:
+        instruction = _input[0]
+    
+    if instruction == "L" or instruction == "R":
+        pos = move(pos, instruction)
+    elif instruction == "D":
+        delete(pos)
+    else:
+        pos = insert(pos, letter)
+
+letters_left.extend(letters_right)
+for letter in letters_left:
+    print(letter, end="")
