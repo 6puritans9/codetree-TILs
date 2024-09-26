4c690f344f035e7d91f09bd65095b6b53c5d7a92
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-09-26 11:23:10 +0900

    Title: 기수 정렬 구현, Time: 276ms, Memory: 37MB, Status: Passed - CodeTree

--- 240926/기수 정렬 구현/implement-radix-sort.py
+++ 240926/기수 정렬 구현/implement-radix-sort.py
@@ -1,18 +1,12 @@
-def radix_sort(numbers, n, max_num_digits):
+def radix_sort(numbers, max_num_digits):
     for i in range(1, max_num_digits + 1):
         digits = [[] for _ in range(10)]
 
-        for j in range(n):
-            number = numbers[j]
+        for number in numbers:
             digit = (number // pow(10, i - 1)) % 10
-
             digits[digit].append(number)
 
-    result = []
-    for digit_list in digits:
-        result.extend(digit_list)
-
-    numbers = result
+        numbers = [num for sublist in digits for num in sublist]
 
     return numbers
 
@@ -21,6 +15,6 @@ n = int(input())
 numbers = list(map(int, input().split()))
 max_num_digits = len(str(max(numbers)))
 
-new_numbers = radix_sort(numbers, n , max_num_digits)
+new_numbers = radix_sort(numbers, max_num_digits)
 for number in new_numbers:
-    print(number, end = " ")
+    print(number, end=" ")
