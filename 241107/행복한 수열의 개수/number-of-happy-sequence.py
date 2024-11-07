52ba3d11e7f7d0db068c130af9488451199886f4
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-11-07 11:57:22 +0900

    Title: 행복한 수열의 개수, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241107/행복한 수열의 개수/number-of-happy-sequence.py
+++ 241107/행복한 수열의 개수/number-of-happy-sequence.py
@@ -3,37 +3,33 @@ grid = [[int(num) for num in input().split()] for _ in range(n)]
 result = 0
 
 for i, row in enumerate(grid):
-    for j, col in enumerate(row):
-        if j <= n - m:
-            sub_grid = row[j:j+m]
-
+    count = 1
+    for j in range(1, n):
+        if row[j] != row[j-1]:
             count = 1
-            for k in range(1, len(sub_grid)):
-                if sub_grid[k] == sub_grid[k - 1]:
-                    count += 1
-                else:
-                    count = 0
-                
-            if count >= m:
-                result += 1
-                break
+            continue
+        
+        count += 1
+    
+        if count >= m:
+            result += 1
+            break
+        
+          
         
 for j in range(n):
-    for i in range(n):
-        col = [grid[i][j] for i in range(n)]
-        
-        if i <= n-m:
-            sub_grid = col[j:j+m]
-
+    col = [grid[i][j] for i in range(n)]
+    
+    count = 1
+    for i in range(1, n):
+        if col[i] != col[i-1]:
             count = 1
-            for k in range(1, len(sub_grid)):
-                if sub_grid[k] == sub_grid[k - 1]:
-                    count += 1
-                else:
-                    count = 0
-            
-            if count >= m:
-                result += 1
-                break
+            continue
 
+        count += 1
+        
+        if count >= m:
+            result += 1
+            break
+         
 print(result)
