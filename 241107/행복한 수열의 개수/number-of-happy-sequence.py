164809d5673a42ff76c84c08501ab02b8095884d
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-11-07 12:06:09 +0900

    Title: 행복한 수열의 개수, Time: 54ms, Memory: 16MB, Status: Passed - CodeTree

--- 241107/행복한 수열의 개수/number-of-happy-sequence.py
+++ 241107/행복한 수열의 개수/number-of-happy-sequence.py
@@ -2,29 +2,32 @@ n, m = map(int, input().split())
 grid = [[int(num) for num in input().split()] for _ in range(n)]
 result = 0
 
-for i, row in enumerate(grid):
-    count = 1
-    for j in range(1, n):
-        if row[j] == row[j - 1]:
-            count += 1
-        else:
-            count = 1
+if m == 1:
+    result = n * 2
+else:
+    for i, row in enumerate(grid):
+        count = 1
+        for j in range(1, n):
+            if row[j] == row[j - 1]:
+                count += 1
+            else:
+                count = 1
 
-        if count >= m:
-            result += 1
-            break
+            if count >= m:
+                result += 1
+                break
 
-for j in range(n):
-    col = [grid[i][j] for i in range(n)]
-    count = 1
-    for i in range(1, n):
-        if col[i] == col[i - 1]:
-            count += 1
-        else:
-            count = 1
+    for j in range(n):
+        col = [grid[i][j] for i in range(n)]
+        count = 1
+        for i in range(1, n):
+            if col[i] == col[i - 1]:
+                count += 1
+            else:
+                count = 1
 
-        if count >= m:
-            result += 1
-            break
+            if count >= m:
+                result += 1
+                break
 
 print(result)
