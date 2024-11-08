bd00f719e126b0de64270e0979a41cbbf4ef22ff
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-11-08 10:05:18 +0900

    Title: 트로미노, Time: 98ms, Memory: 18MB, Status: Passed - CodeTree

--- 241108/트로미노/tromino.py
+++ 241108/트로미노/tromino.py
@@ -1,5 +1,5 @@
 def in_range(x, y, n, m):
-    return 0<= x < n and 0<= y < m
+    return 0 <= x < m and 0 <= y < n
 
 
 def get_sum_1(table, n, m):
@@ -17,31 +17,31 @@ def get_sum_1(table, n, m):
             fig_4_sum = 0
 
             for dy, dx in fig_1:
-                nx, ny = x+ dx, y + dy
+                nx, ny = x + dx, y + dy
                 if not in_range(nx, ny, n, m):
                     break
-                
+
                 fig_1_sum += table[ny][nx]
-            
+
             for dy, dx in fig_2:
-                nx, ny = x+ dx, y + dy
+                nx, ny = x + dx, y + dy
                 if not in_range(nx, ny, n, m):
                     break
-                
+
                 fig_2_sum += table[ny][nx]
 
             for dy, dx in fig_3:
-                nx, ny = x+ dx, y + dy
+                nx, ny = x + dx, y + dy
                 if not in_range(nx, ny, n, m):
                     break
-                
+
                 fig_3_sum += table[ny][nx]
 
             for dy, dx in fig_4:
-                nx, ny = x+ dx, y + dy
+                nx, ny = x + dx, y + dy
                 if not in_range(nx, ny, n, m):
                     break
-                
+
                 fig_4_sum += table[ny][nx]
 
             max_sum = max(max_sum, fig_1_sum, fig_2_sum, fig_3_sum, fig_4_sum)
@@ -52,7 +52,7 @@ def get_sum_1(table, n, m):
 def get_sum_2(table, n, m):
     fig_1 = [(0, -1), (0, 0), (0, 1)]
     fig_2 = [(-1, 0), (0, 0), (1, 0)]
-    
+
     max_sum = 0
     for y in range(n):
         for x in range(m):
@@ -63,16 +63,16 @@ def get_sum_2(table, n, m):
                 nx = x + dx
                 if not in_range(nx, y, n, m):
                     break
-                
+
                 fig_1_sum += table[y][nx]
-            
+
             for dy, dx in fig_2:
                 ny = y + dy
                 if not in_range(x, ny, n, m):
                     break
-                
+
                 fig_2_sum += table[ny][x]
-            
+
             max_sum = max(max_sum, fig_1_sum, fig_2_sum)
 
     return max_sum
