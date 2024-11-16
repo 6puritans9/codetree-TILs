ea5e68df40394bd3f60bf501db9c705065f68cb9
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-11-16 09:40:01 +0900

    Title: 기울어진 직사각형, Time: 131ms, Memory: 20MB, Status: Passed - CodeTree

--- 241116/기울어진 직사각형/slanted-rectangle.py
+++ 241116/기울어진 직사각형/slanted-rectangle.py
@@ -1,48 +1,48 @@
-def in_range(x, y, n):
-    return 0 <= x < n and 0 <= y < n
-
-
-def get_rectangle_sum(x, y):
-    global grid, n
-
-    dxs = [0, 1, 0, -1]
-    dys = [1, 0, -1, 0]
-    rectangle_sum = 0
-
-    for dx, dy in zip(dxs, dys):
-        nx, ny = x + dx, y + dy
-        if not in_range(nx, ny, n):
-            return 0
-
-        rectangle_sum += grid[ny][nx]
-
-    return rectangle_sum
-
-
-
-def get_part_sum(x, y):
-    part_sum = get_rectangle_sum(x, y)
-    nx, ny = x + 1, y - 1
-
-    while in_range(nx, ny, n):
-        rectangle_sum = get_rectangle_sum(nx, ny)
-        if not rectangle_sum:
-            break
-        
-        part_sum += rectangle_sum
-        part_sum -= grid[ny][nx - 1] + grid[ny + 1][nx]  # remove duplicates
-
-        nx, ny = nx + 1, ny - 1
-
-    return part_sum
-
+# 변수 선언 및 입력:
 
 n = int(input())
-grid = [[int(num) for num in input().split()] for _ in range(n)]
-max_sum = 0
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
-for y in range(n):
-    for x in range(n):
-        max_sum = max(max_sum, get_part_sum(x, y))
 
-print(max_sum)
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+
+def get_score(x, y, k, l):
+    dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
+    move_nums = [k, l, k, l]
+    
+    sum_of_nums = 0
+
+    # 기울어진 직사각형의 경계를 쭉 따라가봅니다.
+    for dx, dy, move_num in zip(dxs, dys, move_nums):
+        for _ in range(move_num):
+            x, y = x + dx, y + dy
+                
+            # 기울어진 직사각형이 경계를 벗어나는 경우라면
+            # 불가능하다는 의미로 답이 갱신되지 않도록
+            # 0을 반환합니다.
+            if not in_range(x, y):
+                return 0
+            
+            sum_of_nums += grid[x][y]
+    
+    return sum_of_nums
+
+
+ans = 0
+
+# (i, j)를 시작으로 1, 2, 3, 4 방향
+# 순서대로 길이 [k, l, k, l] 만큼 이동하면 그려지는
+# 기울어진 직사각형을 잡아보는
+# 완전탐색을 진행해봅니다.
+for i in range(n):
+    for j in range(n):
+        for k in range(1, n):
+            for l in range(1, n):
+                ans = max(ans, get_score(i, j, k, l))
+
+print(ans)
