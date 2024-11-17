89f287ae27485ddd1dbf7eb2f4ac0d4273ae55d5
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-11-18 05:54:22 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241118/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 241118/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -1,33 +1,40 @@
-def get_rect_sum(grid, x1, y1, x2, y2):
-    total = 0
-    for i in range(x1, x2 + 1):
-        for j in range(y1, y2 + 1):
-            total += grid[i][j]
-    return total
+def get_rect_sum(grid, y1, x1, y2, x2):
+    rect_sum = 0
+
+    for i in range(y1, y2 + 1):
+        for j in range(x1, x2 + 1):
+            rect_sum += grid[i][j]
+
+    return rect_sum
+
 
 def get_max_sum(grid, n, m):
-    max_sum = float('-inf')
-    
     rectangles = []
-    for x1 in range(n):
-        for y1 in range(m):
-            for x2 in range(x1, n):
-                for y2 in range(y1, m):
-                    rect_sum = get_rect_sum(grid, x1, y1, x2, y2)
-                    rectangles.append((rect_sum, x1, y1, x2, y2))
-    
-    for i in range(len(rectangles)):
-        sum1, x1a, y1a, x2a, y2a = rectangles[i]
-        for j in range(i + 1, len(rectangles)):
-            sum2, x1b, y1b, x2b, y2b = rectangles[j]
+    rectangles_size = 0
+    max_sum = float("-inf")
+
+    for y1 in range(n):
+        for x1 in range(m):
+            for y2 in range(y1, n):
+                for x2 in range(x1, n):
+                    rect_sum = get_rect_sum(grid,y1,x1,y2,x2)
+                    rectangles.append((rect_sum, y1, x1, y2, x2))
+                    rectangles_size += 1
+
+    for i in range(rectangles_size):
+        for j in range(rectangles_size):
+            rect_sum1, y_start1, x_start1, y_end1, x_end1 = rectangles[i]
+            rect_sum2, y_start2, x_start2, y_end2, x_end2 = rectangles[j]
+
+            if y_start1 <= y_end2 and y_end1 >= y_start2 and x_start1 <= x_end2 and x_end1 >= x_start2:
+                continue
             
-            if not (x1a <= x2b and x1b <= x2a and y1a <= y2b and y1b <= y2a):
-                max_sum = max(max_sum, sum1 + sum2)
-    
+            max_sum = max(max_sum, rect_sum1 + rect_sum2) 
+
     return max_sum
+
 
 n, m = map(int, input().split())
 grid = [[int(num) for num in input().split()] for _ in range(n)]
 
-result = get_max_sum(grid, n, m)
-print(result)
+print(get_max_sum(grid, n, m))
