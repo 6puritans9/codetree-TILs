7ef1c0385f6441eee89ef06b6c5aa162cd863f2f
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-11-20 06:28:09 +0900

    Title: 양수 직사각형의 최대 크기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241120/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 241120/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,30 +1,34 @@
-def get_max_size(grid, n, m):
-    max_size = 0
+def preprocess(grid, down_max, n, m):
+    for j in range(m):
+        if grid[n-1][j] > 0:
+            down_max[n-1][j] = 1
 
-    for start_y in range(n):
-        for start_x in range(m):
-            for end_y in range(start_y, n):
-                for end_x in range(start_x, m):
-                    is_valid = True
+    for i in range(n-2, -1, -1):
+        for j in range(m):
+            if grid[i][j] > 0:
+                down_max[i][j] = down_max[i+1][j] + 1
 
-                    for i in range(start_y, end_y + 1):
-                        for j in range(start_x, end_x + 1):
-                            if grid[i][j] <= 0:
-                                is_valid = False
-                                break
 
-                        if not is_valid:
-                            break
+def get_max_area(down_max, n, m):
+    max_area = float("-inf")
 
-                    if is_valid:
-                        area_size = (end_y + 1 - start_y) * (end_x + 1 - start_x)
-                        max_size = max(max_size, area_size)
+    for y in range(n):
+        for x in range(m):
+            max_height = float("inf")
 
-    return max_size if max_size > 0 else -1
+            for l in range(x, m):
+                max_height = min(max_height, down_max[y][l])
+
+                max_area = max(max_area, max_height * (l - x + 1))
+    
+    return max_area
+
 
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
     grid = [[int(num) for num in input().split()] for _ in range(n)]
+    down_max = [[0 for _ in range(m)] for _ in range(n)]
 
-    print(get_max_size(grid, n, m))
+    preprocess(grid, down_max, n, m)
+    print(get_max_area(down_max, n, m))
