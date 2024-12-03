a28dae2685ff9146dd48e3814c7a654a402a62ff
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-12-03 22:09:00 +0900

    Title: 떨어지는 1자 블록, Time: 82ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241203/떨어지는 1자 블록/falling-horizontal-block.py
+++ 241203/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,23 +1,30 @@
 def drop_block(grid, n, m, k):
     y = 0
-    x = k - 1
+
+    if n <= 1:
+        for x in range(k-1, k+m-1):
+            is_occupied = grid[y][x]
+            if is_occupied:
+                return
+        
+        for x in range(k-1, k+m-1):
+                grid[y][x] = 1
+        
+        return
+
     
     for _ in range(n):
         contacted = False
 
-        for i in range(m):
-            below_space = grid[y][x + 1]
+        for x in range(k-1, k+m-1):
+            below_space = grid[y + 1][x]
             if below_space:
                 contacted = True
                 break
             
-            x += 1
-
-        x = k - 1
         if contacted:
-            for i in range(m):
+            for x in range(k-1, k+m-1):
                 grid[y][x] = 1
-                x += 1
             break
 
         y += 1
@@ -27,8 +34,7 @@ if __name__ == "__main__":
     n, m, k = tuple(map(int, input().split()))
     grid = [[int(num) for num in input().split()] for _ in range(n)]
 
-    if n > 1 :
-        drop_block(grid, n, m, k)
-        
+    drop_block(grid, n, m, k)
+
     for row in grid:
         print(" ".join(map(str, row)))
