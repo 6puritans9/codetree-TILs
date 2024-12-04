6fb2279f0428f9d791f03477da25d2e66008644c
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-12-04 17:48:18 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241204/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 241204/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -1,12 +1,30 @@
 def in_range(y, x, n):
     return 0<=y<n and 0<=x<n
 
+
 def is_wall(maze, y, x):
     return maze[y][x] == "#"
 
 
-def turn(cur_idx):
-    return (cur_idx + 1) % 4
+def turn(cur_idx, clockwise):
+    return (cur_idx - 1) % 4 if clockwise else (cur_idx + 1) % 4
+
+
+def should_turn(cur_y, cur_x, dir_idx, maze, n):
+    dy, dx = None, None
+    
+    if dir_idx == 0:
+        dy, dx = cur_y + 1, cur_x
+    elif dir_idx == 1:
+        dy, dx = cur_y, cur_x + 1
+    elif dir_idx == 2:
+        dy, dx = cur_y - 1, cur_x
+    else:
+        dy, dx = cur_y, cur_x - 1
+    
+    if in_range(dy, dx, n):
+        return is_wall(maze, dy, dx)
+    return False
 
 
 def forward(y, x, direction):
@@ -26,17 +44,26 @@ def escape(maze, n, start_y, start_x, visited):
         direction = directions[i]
         ny, nx = forward(y, x, direction)
         
-
         if not in_range(ny, nx, n):
             count += 1
             return count
         
-        elif is_wall(maze, ny, nx):
-            i = turn(i)
-            continue
+        else:        
+            if visited[ny][nx]:
+                break
 
-        elif visited[ny][nx]:
-            break
+            if is_wall(maze, ny, nx):
+                i = turn(i, 0)
+                continue
+
+            elif should_turn(y, x, i, maze, n):
+                # y, x = ny, nx
+                # visited[y][x] = 1
+                # count += 1
+
+                i = turn(i, 1)
+                # ny, nx = forward(y, x, direction)
+
 
         visited[ny][nx] = 1
         y, x = ny, nx
