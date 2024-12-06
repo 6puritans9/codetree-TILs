1256e838e6831dd5ede21e4484553aaecec2f545
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-12-06 21:59:49 +0900

    Title: 뱀은 사과를 좋아해, Time: 52ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241206/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -0,0 +1,88 @@
+from collections import deque
+
+
+class Snake():
+    def __init__(self):
+        self.positions = deque([(0, 0)])
+        self.length = 1
+        self.head = self.positions[0]
+
+    def extend(self, grid, y, x):
+        grid[y][x] = 0
+        self.length += 1
+
+    def shrink(self):
+        self.positions.pop()
+
+    def move(self, grid, y, x):
+        self.positions.appendleft((y, x))
+
+        if grid[y][x]:
+            self.extend(grid, y, x)
+        else:
+            self.shrink()
+
+        self.head = self.positions[0]
+
+
+def in_range(y, x, n):
+    return 0 <= y < n and 0 <= x < n
+
+
+def is_overlapped(snake):
+    # return snake.head in list(snake.positions)[1:]
+    # print("Positions:", list(snake.positions))
+    # print("Head:", snake.head)
+    # print(snake.positions.count(snake.head))
+    return snake.positions.count(snake.head) > 1
+
+
+
+def play(grid, n, snake, direction, distance, count):
+    directions = {"L": (0, -1), "R": (0, 1), "U": (-1, 0), "D": (1, 0)}
+
+    for _ in range(distance):
+        dy, dx = directions[direction]
+        y, x = snake.head
+        ny, nx = y + dy, x + dx
+
+        if not in_range(ny, nx, n):
+            return count
+
+        snake.move(grid, ny, nx)
+        # if is_overlapped(snake):
+        #     print(count)
+        #     return count
+
+        count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    n, m, k = tuple(map(int, input().split()))
+    info_apple = []
+    info_snake = []
+
+    for _ in range(m):
+        _input = input().split()
+        info_apple.append((int(_input[0]), int(_input[1])))
+
+    for _ in range(k):
+        _input = input().split()
+        info_snake.append((_input[0], int(_input[1])))
+
+    grid = [[0 for _ in range(n)] for _ in range(n)]
+    for info in info_apple:
+        y, x = info
+        grid[y - 1][x - 1] = 1
+
+    snake = Snake()
+    count = 0
+    for info in info_snake:
+        direction, distance = info
+        count = play(grid, n, snake, direction, distance, count)
+        if is_overlapped(snake):
+            break
+
+    print(count)
