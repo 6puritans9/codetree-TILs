90fd85a423fdcbcb2e92017874ffccdd454e4585
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-12-15 12:09:25 +0900

    Title: 아름다운 수, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241215/아름다운 수/beautiful-number.py
@@ -0,0 +1,31 @@
+def is_valid(str_num):
+    numbers = [int(char) for char in str_num]
+    result = {}
+
+    for number in numbers:
+        if number not in result:
+            result[number] = 0
+        result[number] += 1
+
+    for number, count in result.items():
+        if number != count:
+            return False
+    
+    return True
+
+
+def find_numbers(str_num, count, n):
+    if len(str_num) == n:
+        if is_valid(str_num):
+            count[0] += 1
+        return
+    
+    for i in range(1, 5):
+        find_numbers(str_num + str(i), count, n)
+
+
+n = int(input())
+string = ""
+count = [0]
+find_numbers(string, count, n)
+print(count[0])
