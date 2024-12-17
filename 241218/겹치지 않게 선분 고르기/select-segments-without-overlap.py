84a84d6d1010e8f993cbe1ebbc564f350092267e
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-12-18 05:43:33 +0900

    Title: 겹치지 않게 선분 고르기, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241218/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 241218/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -1,31 +1,19 @@
-def is_overlap(visited, start2, end2):
-    for start1, end1 in visited:
-        if start1 <= end2 and end1 >= start2:
-            return True
+def max_non_overlapping_intervals(lines):
+    lines.sort(key=lambda x: x[1])
 
-    return False
+    count = 0
+    last_end = float('-inf')
 
+    for start, end in lines:
+        if start >= last_end:
+            count += 1
+            last_end = end
 
-def backtrack(lines, n, idx, visited):
-    global max_count
-
-    if idx == n:
-        max_count = max(max_count, len(visited))
-        return
-
-    start1, end1 = lines[idx]
-    if not is_overlap(visited, start1, end1):
-        visited.add((start1, end1))
-        backtrack(lines, n, idx + 1, visited)
-        visited.remove((start1, end1))
-
-    backtrack(lines, n, idx + 1, visited)
+    return count
 
 
 if __name__ == "__main__":
     n = int(input())
     lines = [tuple(map(int, input().split())) for _ in range(n)]
-    max_count = 0
 
-    backtrack(lines, n, 0, set())
-    print(max_count)
+    print(max_non_overlapping_intervals(lines))
