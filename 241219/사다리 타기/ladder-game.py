ce5f42b6de84b566a7abefb2a417afa0de853b45
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-12-19 11:29:52 +0900

    Title: 사다리 타기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241219/사다리 타기/ladder-game.py
+++ 241219/사다리 타기/ladder-game.py
@@ -1,36 +1,35 @@
-def simulate(n, lines):
-    result = list(range(1, n + 1))
-    for a, b in sorted(lines, key=lambda x: x[1]):
-        result[a - 1], result[a] = result[a], result[a - 1]
+
+def simulate(lines):
+    global n
+
+    result = [i for i in range(n + 1)]
+
+    for start, depth in sorted(lines, key=lambda x: x[1]):
+        result[start], result[start + 1] = result[start + 1], result[start]
+
     return result
 
 
-def backtrack(n, all_lines, target_result, index=0, current_lines=[]):
-    global min_lines
+def backtrack(all_lines, idx, target, cur_lines):
+    global min_cost, m
 
-    if simulate(n, current_lines) == target_result:
-        min_lines = min(min_lines, len(current_lines))
+    if idx == m:
         return
 
-    if index >= len(all_lines) or len(current_lines) >= min_lines:
+    if simulate(cur_lines) == target:
+        min_cost = min(min_cost, len(cur_lines))
         return
 
-    backtrack(n, all_lines, target_result, index + 1, current_lines + [all_lines[index]])
-    backtrack(n, all_lines, target_result, index + 1, current_lines)
-
-
-def find_min_lines(n, m, all_lines):
-    global min_lines
-    min_lines = m 
-    target_result = simulate(n, all_lines)
-
-    backtrack(n, all_lines, target_result)
-    return min_lines
+    backtrack(all_lines, idx + 1, target, cur_lines + [all_lines[idx]])
+    backtrack(all_lines, idx + 1, target, cur_lines)
 
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
     all_lines = [tuple(map(int, input().split())) for _ in range(m)]
-    
-    result = find_min_lines(n, m, all_lines)
-    print(result)
+
+    target = simulate(all_lines)
+    min_cost = float("inf")
+
+    backtrack(all_lines, 0, target, [])
+    print(min_cost)
