c038a12ddf9448da794ceb03e6d989c31efe8ceb
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2024-12-29 16:46:11 +0900

    Title: n개 중에 m개 뽑기, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241229/n개 중에 m개 뽑기/n-choose-m.py
@@ -0,0 +1,21 @@
+def get_combinations(n, m, start, array):
+    global answers
+
+    if len(array) == m:
+        number = "".join(map(str, array))
+        answers.append(number)
+        return
+
+    for i in range(start, n+1):
+        array.append(i)
+        get_combinations(n, m, i + 1, array)
+        array.pop()
+
+
+if __name__ == "__main__":
+    n, m = tuple(map(int, input().split()))
+    answers = []
+    get_combinations(n, m, 1, [])
+
+    for elem in answers:
+        print(" ".join(elem))
