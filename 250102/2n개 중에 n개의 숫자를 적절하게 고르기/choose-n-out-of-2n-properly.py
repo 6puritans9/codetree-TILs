c5a1a04835a6cf08bf2f7641afc78a4ecf283b5d
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2025-01-02 23:29:55 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250102/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ 250102/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -1,19 +1,15 @@
 def backtrack(numbers, n, idx, sub_group, min_diff):
     if len(sub_group) == n:
         other_group = [number for number in numbers if number not in sub_group]
-        # print(other_group)
         cur_diff = abs(sum(sub_group) - sum(other_group))
-        # print(cur_diff)
-        # print("--------")
         min_diff[0] = min(min_diff[0], cur_diff)
         return
     if idx >= n*2:
         return
     
-    for i in range(idx, n*2):
-        sub_group.append(numbers[i])
-        backtrack(numbers, n, i + 1, sub_group, min_diff)
-        sub_group.pop()
+    sub_group.append(numbers[idx])
+    backtrack(numbers, n, idx + 1, sub_group, min_diff)
+    sub_group.pop()
     
     backtrack(numbers, n , idx + 1, sub_group, min_diff)
 
