df90526ab4272f90b8655b9715e6b7e82a72b000
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2025-01-05 20:48:24 +0900

    Title: 외판원 순회, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250105/외판원 순회/traveling-salesman-problem.py
+++ 250105/외판원 순회/traveling-salesman-problem.py
@@ -1,6 +1,8 @@
 def backtrack(costs, n, frm, to, min_cost, cur_cost, cnt):
     global visited
 
+    if cur_cost >= min_cost[0]:
+        return
     if cnt == n - 1:
         cur_cost += costs[frm][0]
         min_cost[0] = min(min_cost[0], cur_cost)
@@ -8,11 +10,12 @@ def backtrack(costs, n, frm, to, min_cost, cur_cost, cnt):
     if visited[to]:
         return
 
-    visited[to] = True
-    cost = cur_cost + costs[frm][to]
-    for i in range(1, n):
-        backtrack(costs, n, to, i, min_cost, cost, cnt + 1)
-    visited[to] = False
+    if costs[frm][to]:
+        visited[to] = True
+        cost = cur_cost + costs[frm][to]
+        for i in range(1, n):
+            backtrack(costs, n, to, i, min_cost, cost, cnt + 1)
+        visited[to] = False
 
 
 def get_min_cost(costs, n):
