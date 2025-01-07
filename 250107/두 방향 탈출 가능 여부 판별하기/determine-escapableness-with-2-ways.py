f325db45990babe6e37409242422bdfd277ca090
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2025-01-07 12:19:29 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 59ms, Memory: 17MB, Status: Passed - CodeTree

--- 250107/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 250107/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,31 +1,29 @@
-def in_range(y, x, n):
-    return 0 <= y < n and 0 <= x < n
+def in_range(y, x, n, m):
+    return 0 <= y < n and 0 <= x < m
 
 
-def dfs(grid, n, visited, y, x):
-    if y == n - 1 and x == n - 1:
-            return True
-    
+def dfs(grid, n, m, visited, y, x):
+    if y == n - 1 and x == m - 1:
+        return True
+
     dys = [0, 1]
     dxs = [1, 0]
-
     for dy, dx in zip(dys, dxs):
         ny, nx = y + dy, x + dx
 
-        if in_range(ny, nx, n):
-            if grid[ny][nx] and not visited[ny][nx]:
-                visited[ny][nx] = True
-                if dfs(grid, n, visited, ny, nx):
-                    return True
-    
+        if in_range(ny, nx, n, m) and grid[ny][nx] and not visited[ny][nx]:
+            visited[ny][nx] = True
+            if dfs(grid, n, m, visited, ny, nx):
+                return True
+
     return False
 
 
 def find_exit(grid, n, m):
-    visited = [[False for _ in range(n)] for _ in range(n)]
+    visited = [[False for _ in range(m)] for _ in range(n)]
     visited[0][0] = True
 
-    is_escapable = dfs(grid, n, visited, 0, 0)
+    is_escapable = dfs(grid, n, m, visited, 0, 0)
     return 1 if is_escapable else 0
 
 
