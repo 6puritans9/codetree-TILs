5309ba69fef34cc8438823b7ee1013151c7ffc22
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2025-01-10 16:58:50 +0900

    Title: 뿌요뿌요, Time: 155ms, Memory: 23MB, Status: Passed - CodeTree

--- 250110/뿌요뿌요/puyo-puyo.py
+++ 250110/뿌요뿌요/puyo-puyo.py
@@ -1,15 +1,17 @@
-def in_range(y, x, n):
+from typing import List, Tuple
+
+def in_range(y:int, x:int, n:int) -> bool:
     return 0<=y<n and 0<=x<n
 
 
-def bayoen(grid, coords):
+def bayoen(grid:List[List[int]], coords:List[Tuple[int, int]]) -> bool:
     for y, x in coords:
         grid[y][x] = 0
 
     return True
 
 
-def visit_and_blast(grid, y, x, n):
+def visit_and_blast(grid:List[List[int]], y:int, x:int, n:int) -> Tuple[bool, int]:
     dys = [-1, 0, 1, 0]
     dxs = [0, 1, 0, -1]
     
@@ -40,10 +42,10 @@ def visit_and_blast(grid, y, x, n):
     if len(coords) >= 4:
         did_blast = bayoen(grid, coords)
     
-    return [did_blast, cur_block_size]
+    return did_blast, cur_block_size
     
 
-def play(grid, n):
+def play(grid:List[List[int]], n:int) -> Tuple[int, int]:
     blasted_count = 0
     biggest_block = 0
 
@@ -51,12 +53,11 @@ def play(grid, n):
         for x in range(n):
             if grid[y][x]:
                 did_blast, block_size = visit_and_blast(grid, y, x, n)                
-                if did_blast:
-                    blasted_count += 1
-                if block_size > biggest_block:
-                    biggest_block = block_size
+                
+                blasted_count += 1 if did_blast else 0
+                biggest_block = max(biggest_block, block_size)
                     
-    return [blasted_count, biggest_block]
+    return blasted_count, biggest_block
 
 
 if __name__ == "__main__":
@@ -64,4 +65,3 @@ if __name__ == "__main__":
     grid = [list(map(int, input().split())) for _ in range(n)]
 
     print(" ".join(map(str, play(grid, n))))
-    # print(grid)
