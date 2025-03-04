452002bfd4f6e9b386e483dc7d62ae71a281b374
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2025-03-04 12:21:20 +0900

    Title: 체크판위에서 2, Time: 53ms, Memory: 16MB, Status: Passed - CodeTree

--- 250304/체크판위에서 2/on-the-checkboard-2.py
+++ 250304/체크판위에서 2/on-the-checkboard-2.py
@@ -1,10 +1,10 @@
 def find_paths(r:int, c:int, grid:list[list[str]]) -> int:
-    dp = [[[0 for _ in range(3)]for _ in range(c)] for _ in range(r)]
+    dp = [[[0 for _ in range(4)]for _ in range(c)] for _ in range(r)]
     dp[0][0][0] = 1
     
     for y in range(r):
         for x in range(c):
-            for step in range(3):
+            for step in range(4):
                 if dp[y][x][step] == 0:
                     continue
 
@@ -13,10 +13,17 @@ def find_paths(r:int, c:int, grid:list[list[str]]) -> int:
                         if grid[y][x] != grid[ny][nx]:
                             new_step = step + 1
 
-                            if new_step < 3:
-                                dp[ny][nx][new_step] = max(dp[ny][nx][step], dp[y][x][step] + 1)
+                            if new_step >= 4:
+                                continue
 
-    return max(dp[r-1][c-1])
+                            elif new_step == 3 and (ny != r-1 or nx != c-1):
+                                continue
+
+                            else:
+                                dp[ny][nx][new_step] += dp[y][x][step]
+
+    return dp[r-1][c-1][3]
+    
 
 if __name__ == "__main__":
     r, c = map(int, input().split())
