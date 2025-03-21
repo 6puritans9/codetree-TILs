9293bac1a7900f7911fefd721d44ea6834533b5b
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2025-03-21 12:40:04 +0900

    Title: 바구니 안의 사탕 2, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250321/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250321/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,13 +1,19 @@
 def find_max_candies(n:int, k:int, baskets:list[int, int]) -> int:
-    global MAX_POS
-    max_candies = 0
+    # TC = O(NlogN) + O(N) = O(NlogN)
+    # SC = O(1)
     
-    for c in range(MAX_POS + 1):
-        cur_candies = 0
+    max_candies = 0
+    cur_candies = 0
+    
+    left = 0
+    for right in range(n):
+        pos, right_candies = baskets[right]
+        cur_candies += right_candies
 
-        for pos, candies in baskets:
-            if c-k <= pos <= c+k:
-                cur_candies += candies
+        while pos - baskets[left][0] > k * 2:
+            left_candies = baskets[left][1]
+            cur_candies -= left_candies
+            left += 1
         
         max_candies = max(max_candies, cur_candies)
 
@@ -32,14 +38,12 @@ if __name__ == "__main__":
     # 3. for c in range(k, n-k), add candies and compare to the max_candies
     # return max_candies
 
-    MAX_POS = 100
-
     n, k = map(int, input().split())
     baskets = []
     for _ in range(n):
         candies, pos = map(int, input().split())
         baskets.append((pos, candies))
 
-    # baskets.sort(key=lambda x:x[0])
+    baskets.sort(key=lambda x:x[0])
     
     print(find_max_candies(n, k, baskets))
