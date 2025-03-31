c5c43fa65588dd5cef9d8d6fdd7cf258ce65bd9b
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2025-03-31 15:26:27 +0900

    Title: 최대 H 점수 2, Time: 51ms, Memory: 17MB, Status: Passed - Codetree

--- 250331/최대 H 점수 2/maximum-h-score-2.py
+++ 250331/최대 H 점수 2/maximum-h-score-2.py
@@ -1,42 +1,25 @@
-def get_h_index(n: int, numbers: list[int]) -> int:
-    max_number = max(numbers)
-    arr = [0 for _ in range(max_number + 1)]
+def greedy_max_h_index(n: int, l: int, citations: list[int]) -> int:
+    # TC = O(100 * N) = O(10^2 * 10^2) == 500ms
+    # SC = O(1)
 
-    for number in numbers:
-        arr[number] += 1
+    MAX_INDEX = 100
 
-    papers = 0
-    for h_index in range(max_number, -1, -1):
-        papers += arr[h_index]
+    max_h_index = 0
+    for i in range(1, MAX_INDEX + 1):
+        h_index = 0
+        added = 0
 
-        if papers >= h_index:
-            return h_index
+        for citation in citations:
+            if citation >= i:
+                h_index += 1
+            elif citation + 1 >= i and added < l:
+                h_index += 1
+                added += 1
 
-    return 0
+        if h_index >= i:
+            max_h_index = i
 
-
-def increase_l_elements(n, l, numbers, start, count):
-    if count > l:
-        return get_h_index(n, numbers)
-    if start == n:
-        return 0
-
-    max_h = 0
-
-    for i in range(start, n):
-        numbers[i] += 1
-        max_h = max(max_h, increase_l_elements(n, l, numbers, i + 1, count + 1))
-        numbers[i] -= 1
-    max_h = max(max_h, increase_l_elements(n, l, numbers, start + 1, count))
-
-    return max_h
-
-
-def find_max_h(n: int, l: int, numbers: list[int]) -> int:
-    if not l:
-        return get_h_index(n, numbers)
-
-    return increase_l_elements(n, l, numbers, 0, 1)
+    return max_h_index
 
 
 if __name__ == "__main__":
@@ -56,15 +39,17 @@ if __name__ == "__main__":
 
     # Approach
     # 1. I can get h-index for O(NK)
-    #   which will increase to O(NK * nCl) == O(10^4 * 10^4) = 8/8 * 10^3ms
-    # 2. This will occupy O(100) in space, which is easily managable
+    #   which will increase to O(N * C(N,1)) = O(N * (N!/(L! * (N-1)!)))
+    #   that I cannot solve in given time.
+    # 2. This will occupy O(100) in memory, which is easily manageable in space though
 
-    # 3. Add 1 for L selected elements in nCl combination
-    # 4. assign a counting sort array
-    # 5. compare each result to find max_h
-    # 6. return max_h
+    # 3. Since the problem only allows to add 1 if possible, this can be solved in greedy approach
+    # 4. for i in range(10^2 + 1), find the maximum i, which will be the largest h-index possible
+    # 5. for each i, iterate in given numbers, or rather citations,
+    # 6. check if +1 can make the valid cases, up to l
+    # 7. return max_h_index
 
     n, l = map(int, input().split())
-    numbers = [int(num) for num in input().split()]
+    citations = [int(num) for num in input().split()]
 
-    print(find_max_h(n, l, numbers))
+    print(greedy_max_h_index(n, l, citations))
