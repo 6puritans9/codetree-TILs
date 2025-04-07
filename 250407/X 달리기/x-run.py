f07b302dfbdb126c6316b30112bdce1fda9862f8
Lee Kyuhong <134516260+6puritans9@users.noreply.github.com>
2025-04-07 13:11:11 +0900

    Title: X 달리기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250407/X 달리기/x-run.py
+++ 250407/X 달리기/x-run.py
@@ -1,44 +1,21 @@
-from collections import deque
-
-
-def is_valid(speed:int):
-    return speed > 0
-
-
-def bfs(x:int, initial_state:tuple[int]) -> int:
-    # TC = O(MAX_BOUNDARY^2) = O(X^2) = O(10^8) == 1000ms
-    # SC = O(X^2) = O(10^8) = 1bytes *10^2 * (10^3)^2  == 100MB
-
-    MAX_DIST = 2*x
-    MAX_SPEED = 100
-    
-    queue = deque([initial_state])
-    visited = [[False for _ in range(MAX_SPEED)] for _ in range(MAX_DIST)]
-
-    while queue:
-        dist, speed, time = queue.popleft()
-        if dist == x and speed == 1:
-            return time + 1
-
-        if dist >= MAX_DIST or speed >= MAX_SPEED:
-            continue
-        if visited[dist][speed]:
-            continue
-        visited[dist][speed] = True
-
-        if is_valid(speed - 1):
-            queue.append((dist+speed, speed - 1, time + 1))
-        queue.append((dist+speed, speed, time + 1))
-        queue.append((dist+speed, speed + 1, time + 1))
-
-    return -1
-
-
 def find_min_time(x:int) -> int:
-    initial_state = (0, 1, 0)
+    left_dist = x
+    cur_speed = 1
+    time = 0
 
-    return bfs(x, initial_state)
-    
+    while left_dist:
+        time += 1
+        left_dist -= cur_speed
+        
+        if left_dist >= (cur_speed+1)*(cur_speed+2)/2:
+            cur_speed += 1
+        elif left_dist >= cur_speed*(cur_speed+1)/2:
+            continue
+        else:
+            cur_speed -= 1
+        
+    return time
+        
 
 if __name__ == "__main__":
     # 1. Going to run for X length
